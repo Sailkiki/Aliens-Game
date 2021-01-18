71f1eef4a91972f9ff433ebd60dab176009f0c21
Mingzhe Sui <61177530+Sailkiki@users.noreply.github.com>
2021-01-18 13:35:49 +0800

    第二次更新

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,20 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                     ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     while True:
-
+        screen.fill(ai_settings.bg_color)
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
         # 让绘制的屏幕可见
         pygame.display.flip()
