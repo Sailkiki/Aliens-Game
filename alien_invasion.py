1e11a4f399d5ffbb95ae7d8a07e2b2761d57387b
Mingzhe Sui <61177530+Sailkiki@users.noreply.github.com>
2021-01-19 09:54:55 +0800

    添加了飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
-
+from ship import Ship
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -11,11 +11,10 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,
                                      ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    ship = Ship(screen)
     while True:
         screen.fill(ai_settings.bg_color)
-        # 监视键盘和鼠标事件
-
+        ship.blitme()
         # 让绘制的屏幕可见
         pygame.display.flip()
 
